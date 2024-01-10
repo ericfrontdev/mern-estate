592469e649330ec40369379fd752d5a2cef79daa
ericfrontdev <eouellette@videotron.ca>
2024-01-10 14:48:27 -0500

    create server and add mongodb database

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { FaSearch } from "react-icons/fa"
 import { Link } from "react-router-dom"
 
-export default function Header() {
+function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -38,3 +38,5 @@ export default function Header() {
     </header>
   )
 }
+
+export default Header
